c6c8e3bc034c834af5c2565eb39dc01d8bcbd577
Tottibero <tottiesp@gmail.com>
2024-02-11 18:27:26 +0100

    añadido front en el back

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
-
 
 // Importa las rutas
 const heavymusichqRoute = require('./routes/heavymusichq');
@@ -18,8 +17,13 @@ app.use(boolintunesRoute);
 app.use(lambgoat);
 app.use(metalstorm);
 
-
-
+// Sirve los archivos estáticos después de configurar las rutas de la API
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+console.log(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+// Captura todas las demás rutas y sirve el index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+});
 
 const port = 3000;
 
